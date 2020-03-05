d0d7d9bcfdd540b027e87da8e55711a57eaa966c
udaypydi <udaypydi333@gmail.com>
2020-03-05 21:31:14 +0530

    fix(line): check added for vertical line

--- linechallenge.js
+++ linechallenge.js
@@ -3,7 +3,9 @@ const slopeMapper = {};
 function findLine(point1, point2) {
     if (point1.y === point2.y && point1.x === point2.x) {
         console.log('points are same cannot draw a line for', `(${point1.x},${point1.y})`, `(${point2.x},${point2.y})`);
-    } else {
+    } else if (point1.x === point2.x) {
+        console.log('Its a vertical line.', `(${point1.x},${point1.y})`, `(${point2.x},${point2.y})`);
+    }else {
         const m = (point2.y - point1.y) / (point2.x - point1.x)
         const b = (point1.y - m * point1.x);    
         if (slopeMapper[`${m},${b}`]) {
@@ -18,13 +20,16 @@ function findLine(point1, point2) {
 
 // edit these points for new test cases
 
-const points = [{ x: 0, y: 0 }, { x: 1, y: 1 }, { x: 3, y: 3 }, { x: 2, y: 2 }];
+const points = [{ x: 0, y: 0 }, { x: 2, y: 1 }, { x: 3, y: 3 }, { x: 2, y: 2 }];
 
 for (let i = 0 ; i < points.length; i ++) {
     for (let j = 1; j < points.length; j ++) {
         findLine(points[i], points[j]);
     }
 }
+/**
+ * Loop through the keys to check if the line passes more than two points.
+ */
 
 Object.keys(slopeMapper).forEach(key => {
     if (slopeMapper[key] > 2) {
